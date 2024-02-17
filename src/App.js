69df341f42363c7c1dd24e34c784b17d0fc56b0a
Deepjyot Singh Kapoor <kapoordeepjyotsingh29@gmail.com>
2024-02-17 18:40:43 -0500

    this is the first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title,updateTitle] = useState('This is the default title.');
+  const [inputVal,updateInputVal] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>{title}</h1>
+    <h1>{inputVal}</h1>
+    <input type="text" value = {inputVal} onChange={e=>updateInputVal(e.target.value)}/>
+    <button type="button" onClick={()=>updateTitle("New Title")}> Click Button to update title</button>
+
     </div>
   );
 }
